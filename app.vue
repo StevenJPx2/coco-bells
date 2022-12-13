3aed978b9a040cd6096d427f8c4c25bb10836f90
Steven John <stevenjpx2@gmail.com>
2022-12-13 12:16:33 +0530

    feat!: finish website

--- app.vue
+++ app.vue
@@ -1,57 +1,71 @@
 <script setup lang="ts">
-import Lenis from "@studio-freight/lenis";
+useHead({ title: "Coco Bells" });
 
-tryOnMounted(() => {
-  const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-    direction: "vertical", // vertical, horizontal
-    gestureDirection: "vertical", // vertical, horizontal, both
-    smooth: true,
-    mouseMultiplier: 1,
-    smoothTouch: false,
-    touchMultiplier: 2,
-    infinite: false,
-  });
-
-  //get scroll value
-  lenis.on(
-    "scroll",
-    ({
-      scroll,
-      limit,
-      velocity,
-      direction,
-      progress,
-    }: {
-      scroll: number;
-      limit: number;
-      velocity: number;
-      direction: number;
-      progress: number;
-    }) => {
-      console.log({ scroll, limit, velocity, direction, progress });
-    }
-  );
-
-  function raf(time: number) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-
-  requestAnimationFrame(raf);
-});
+const links = {
+  church: { url: "https://wccbangalore.org" },
+  blog: { url: "https://blog.saftapologetics.com" },
+} as const;
 </script>
 
 <template>
-  <div class="h-[200vh] w-screen grid">
-    <h1 class="place-self-center">Coco Bells</h1>
+  <div class="min-h-screen w-screen grid" id="bg">
+    <div class="container grid grid-cols-1 lg:grid-cols-2 items-center gap-8">
+      <svg-icon
+        name="logo"
+        class="w-96 col-start-1 !flex mx-auto justify-self-center"
+      />
+      <div class="lg:col-start-2 text-white row-start-2 lg:row-start-1">
+        <h1 class="font-rochester">Merry Christmas and happy sipping!</h1>
+        <p class="font-libre mt-7">
+          Thank you for stopping by Coco Bells. If you ever want to drop in on a
+          Sunday and catch up with us and listen to a soulful message on faith,
+          hope and love,
+          <nuxt-link :to="links.church.url">
+            our church is always open for you.
+          </nuxt-link>
+          We can't wait to see you again! <br />
+          <nuxt-link :to="links.blog.url">
+            For thought provoking and blissful reading on Christmas, Christ and
+            Christianity, click here!
+          </nuxt-link>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 h1 {
-  @apply text-9xl;
-  @apply font-bold;
+  @apply text-7xl;
+}
+
+p {
+  @apply text-2xl;
+  @apply leading-[1.75];
+}
+
+a {
+  @apply p-1;
+  @apply bg-white/40;
+  @apply rounded;
+  @apply transition-colors;
+
+  &:hover {
+    @apply bg-white;
+    @apply text-[#75090f];
+  }
+}
+
+.container {
+  width: min(100%, 1200px);
+  @apply mx-auto;
+  @apply p-5;
+}
+
+#bg {
+  background: linear-gradient(0deg, #75090f, #75090f),
+    linear-gradient(270.07deg, #f32e6c 15.46%, rgba(243, 47, 108, 0) 93.07%),
+    url(~/assets/raspberry-lace.gif);
+  background-blend-mode: color, normal, normal;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,27 @@
 export default defineNuxtConfig({
   modules: [
     "@nuxtjs/tailwindcss",
-    "@nuxtjs/fontaine",
+    "nuxt-font-loader",
     "@vueuse/nuxt",
     "@nuxt/image-edge",
     "nuxt-icon",
+    "nuxt-svg-transformer",
   ],
+
+  fontLoader: {
+    external: [
+      {
+        src: "https://fonts.googleapis.com/css2?family=Rochester&display=swap",
+        fallback: "cursive",
+        family: "Rochester",
+        class: "font-rochester",
+      },
+      {
+        src: "https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap",
+        fallback: "serif",
+        family: "Libre Baskerville",
+        class: "font-libre",
+      },
+    ],
+  },
 });
